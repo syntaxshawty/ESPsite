3317f916496f87096c860eee682ab88bea4942fd
Mollie Davidson <mdavidson13@fordham.edu>
2021-03-29 02:01:49 -0400

    styling

--- move-logo.js
+++ move-logo.js
@@ -1,19 +1,3 @@
-//LOAD PERCENTAGE
-
-// var elem = document.getElementById("load-count");
-// console.log(elem);
-// var width = 0;
-// var id = setInterval(frame, 50);
-// function frame() {
-//   if (width >= 100) {
-//     clearInterval(id);
-//   } else {
-//     width++;
-//     // elem.style.width = width + "%";
-//     elem.innerHTML = "[" + width * 1 + "/100]";
-//   }
-// }
-
 //MOVE LOGO
 
 // $(".logo-load").click(function () {
